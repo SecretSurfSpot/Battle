1a91c84238d6f407567e1022ea6aaa1742e38185
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-24 11:19:57 +0100

    minor changes to _spec files

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,6 @@ class Game
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
-
   end
 
   def attack(player)
--- spec/game_rspec.rb
+++ spec/game_rspec.rb
@@ -1,16 +1,13 @@
 require "game"
 
 describe Game do
-  game = Game.new
-  player_1 = double
-  player_2 = double
-
+  subject(:game) { described_class.new }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
 
   describe "#attack_player" do
-    it "reduced hit points" do
-      # player_1.attack_player(player_2)
+    it "damages the player" do
       expect(player_2).to receive(:receive_damage)
-      #  expect(mittens).to receive(:receive_damage)
       game.attack_player(player_2)
     end
   end
